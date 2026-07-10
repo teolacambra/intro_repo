27fd76d82a874bd3b360c4ec78b50603d485c1b9
teolacambra <teolacambra@gmail.com>
2026-07-09 21:13:35 -0700

    added new sinewav file and modified fftvis for anc of simple sine with graphs

---
+++ fftvisualization.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import soundfile as sf
+import sounddevice as play
+# pure sine
+# Parameters
+fs = 44100          # Sample rate (Hz)
+duration = 10.0      # seconds
+f1 = 440             # Frequency (Hz)
+A1 = 1.0             # Amplitude
+
+# Generate sine wave
+t = np.arange(0, duration, 1/fs)
+y = (A1 * np.sin(2 * np.pi * f1 * t)) 
+sf.write("440hz_sine.wav", y,fs)
+
+# Generate phase shifted cancellation
+ydelayed = np.zeros_like(y)
+
+delay = 1/(2*f1)              # half period = 180 degrees
+delayinsamples = int(delay*fs)
+
+ydelayed[delayinsamples:] = -y[:-delayinsamples]
+
+# Error signal
+yresult = y + ydelayed
+# FFT of resultat signal
+Y = np.fft.rfft(yresult)
+freq = np.fft.rfftfreq(len(yresult), d=1/fs)
+
+# Normalize
+mag = np.abs(Y) / len(yresult)
+mag[1:-1] *= 2
+
+# Create one window with two plots
+fig, ax = plt.subplots(2, 1, figsize=(12, 8))
+
+# Time-domain plot
+ax[0].plot(t, y, label="original 440 hz sine wave")
+ax[0].plot(t, ydelayed, linestyle="--", label="shifted cancellation wave")
+ax[0].plot(t, yresult, label="error")
+ax[0].set_xlim(0, 0.01)
+ax[0].set_title("Time Domain")
+ax[0].set_xlabel("Time (s)")
+ax[0].set_ylabel("Amplitude")
+ax[0].grid(True)
+
+# Frequency-domain plot
+ax[1].plot(freq, mag)
+ax[1].set_xlim(0, fs/2)
+ax[1].set_title("Frequency Domain")
+ax[1].set_xlabel("Frequency (Hz)")
+ax[1].set_ylabel("Amplitude")
+ax[1].grid(True)
+
+plt.tight_layout()
+plt.show()
+
