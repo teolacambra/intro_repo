c85588e5b6a2c669cf74ca5d32147e47927b24be
teolacambra <teolacambra@gmail.com>
2026-07-10 23:01:44 -0700

    made separate files for pure sine error and pure sine impulse work, will be merged later

--- ancpuresinewithimpulse.py
+++ ancpuresinewithimpulse.py
@@ -13,8 +13,27 @@ A1 = 1.0             # Amplitude
 
 # Generate sine wave
 t = np.arange(0, duration, 1/fs)
-y = (A1 * np.sin(2 * np.pi * f1 * t))
+ypure = (A1 * np.sin(2 * np.pi * f1 * t))
 # export it via: sf.write("440hz_sine.wav", y,fs)
+
+# Simulate an impulse response g(t)
+g = np.zeros_like(ypure)
+g[5] = 1.00 # direct sound with delay by 5 samples
+g[18] = 0.45 # wall reflection
+g[34] = 0.25
+g[57] = 0.12
+ 
+
+# import received recording from microphone recording
+y = scipy.signal.fftconvolve(ypure, g, mode = 'full') # y here is y measured at the mic
+y = y[:len(ypure)]
+# deconvolve y with recording to get impulse response g(t)
+eps = 1e-9 #prevents div 0
+nf = len(y)
+F_measured = np.fft.rfft(y,nf)
+F_pure = np.fft.rfft(ypure,nf)
+G = F_measured / (F_pure + eps)
+g_est = np.fft.irfft(G)
 # Generate phase shifted cancellation
 ydelayed = np.zeros_like(y)
 
@@ -56,6 +75,16 @@ ax[1].set_title("FFT of resultant")
 ax[1].set_xlabel("Frequency (Hz)")
 ax[1].set_ylabel("Amplitude")
 ax[1].grid(True)
+
+# Time-domain plot if impulse
+ax[2].plot(t, g, label = "actual simulated impulse")
+ax[2].plot(t, g_est, "-",label="Impulse guess after deconvolution")
+ax[2].set_xlim(0, 80/fs)
+ax[2].set_title("FFT of resultant")
+ax[2].set_xlabel("Time (s)")
+ax[2].set_ylabel("Amplitude - dBSPL")
+ax[2].grid(True)
+ax[2].legend()
 
 plt.tight_layout()
 plt.show()
